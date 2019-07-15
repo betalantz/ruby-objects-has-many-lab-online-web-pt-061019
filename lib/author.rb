6db0e1f766f1eb9586fde6fdca2629bc6413e5e2
Lantz Warrick <betalantz@gmail.com>
2019-07-15 02:30:50 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,5 +1,6 @@
 class Author
 
+  @@posts = []
   attr_accessor :name, :posts
   def initialize(name)
     @name = name
@@ -8,11 +9,16 @@ class Author
 
   def add_post(post)
     @posts << post
+    @@posts << post
     post.author = self
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
     self.add_post(post)
+  end
+
+  def self.post_count
+    @@post.count
   end
 end
